04b542665cbc44065e24a329b105b5489e509339
agustinVon <agustin.von@ing.austral.edu.ar>
2020-06-01 21:12:28 -0300

    asdasd

---
+++ src/RedBlackTree/RedBlackNode.java
@@ -0,0 +1,21 @@
+package RedBlackTree;
+
+class RedBlackNode
+{
+    RedBlackNode left, right;
+    int element;
+    int color;
+
+    public RedBlackNode(int theElement)
+    {
+        this( theElement, null, null );
+    }
+
+    public RedBlackNode(int theElement, RedBlackNode lt, RedBlackNode rt)
+    {
+        left = lt;
+        right = rt;
+        element = theElement;
+        color = 1;
+    }
+}
